4941cb40b8683cb99254f2bcc2551427fd7fbd41
jrkim-kr <jeongrankim99@gmail.com>
2025-10-24 23:07:57 +0900

    docs: 13강 실습 코드 업데이트

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, () => {
+const server = app.listen(8080, () => {
   console.log("서버 실행중 http://localhost:8080");
 });
 
 app.get("/", (req, res) => {
-  res.send("테스트");
+  res.send("현재 시간은 " + new Date().toISOString() + " 입니다.");
+});
+
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("HTTP server closed");
+  });
+});
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("HTTP server closed");
+  });
 });
